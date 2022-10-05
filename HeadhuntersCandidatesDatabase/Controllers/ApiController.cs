49a6f921dad9feebfe153edd86419b1a375d9f3e
Gvido <gaca6817@gmail.com>
2022-10-05 14:44:16 +0300

    Api controller added

---
+++ HeadhuntersCandidatesDatabase/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using HeadhuntersCandidatesDatabase.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeadhuntersCandidatesDatabase.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test/{id}")]
+        public IActionResult GetCandidate(int id)
+        {
+            var candidate = new Candidate() { Id = 0, FullName = "Gvido Andis Čams" };
+            return Ok(candidate);
+        }
+    }
+}
---
+++ HeadhuntersCandidatesDatabase/Models/Candidate.cs
@@ -0,0 +1,8 @@
+﻿namespace HeadhuntersCandidatesDatabase.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
